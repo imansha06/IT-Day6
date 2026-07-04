7bcfe1d904bbbda166433803fa990b800acc1fd5
Divya Imansha <imanshaaa06@gmail.com>
2026-07-04 12:13:06 +0530

    increase the count with clicked times

--- app.js
+++ app.js
@@ -1,50 +1,13 @@
-//console.log("Hii");
-//class Customer{
-//  name;
-//   age;
-//  address;
+let heading = document.getElementById("heading");
 
-//  constructor(name, age, address){
-//      this.name = name;
-//      this.age = age;
-//      this.address = address;
-//  }
-//}
+heading.innerText = "Rose";
+// heading.innerText = "Rose";
 
-//let customer1 = new Customer("Sharadha", 20, "Bangalore");
-//console.log(customer1);
-
-// ---------------------------------------------
-
-//let customer2= {
-//   name: "John",
-//  age: 25,
-//  address: "New York"
-//};
-//console.log(customer2);
-
-
-
-let customer2={
-    name: "John",
-    age: 25,
-    address: "New York",
-    salary: 50000,
-    items:["item1","item2","item3"],
-    father:{
-        name: "Doe",
-        age: 50,
-        address: "New  York"
-    }
-};
-
-
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.items[2]);
-console.log(customer2.father.name);
-
-
-
+// console.log(heading.);
+let number = 0;
+function changeHeading() {
+    heading.innerText = "Rose"+number++;
+    console.log("Clicked");
+    
+}
 
