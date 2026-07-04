66e62cb9ad7ba8628f28a90fe678ad4fe0cb9435
Divya Imansha <imanshaaa06@gmail.com>
2026-07-04 12:54:15 +0530

    display the text we enter

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-let heading = document.getElementById("heading");
-
-heading.innerText = "Rose";
-// heading.innerText = "Rose";
-
-// console.log(heading.);
-let number = 0;
 function changeHeading() {
-    heading.innerText = "Rose"+number++;
-    console.log("Clicked");
-    
-}
 
-function changeHeading2() {
-    heading.innerText = "Rose"+ --number;
-    console.log("Clicked");
+    let txtInput = document.getElementById("inputText");
+   
+
+    let userInput = txtInput.value; 
+
+    console.log(userInput);
     
+    let heading = document.getElementById("heading");
+
+    heading.innerText=userInput;
+
 }
