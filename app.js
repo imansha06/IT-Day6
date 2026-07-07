7c217b17aeb69fcbce82116ef7e6bdf0af76b4c2
Divya Imansha <imanshaaa06@gmail.com>
2026-07-07 11:02:18 +0530

    commit 8

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+// let customerList = [];
+
+// function btnAddCustomerOnAction(){
+//     let txtName = document.getElementById("txtName").value;
+//     let txtAddress = document.getElementById("txtAddress").value;
+//     let txtAge = document.getElementById("txtAge").value;
+//     let txtEmail = document.getElementById("txtEmail").value;
+//     let txtSalary = document.getElementById("txtSalary").value;
+
+//     let customer={
+//         name:txtName,
+//         address:txtAddress,
+//         age:txtAge,
+//         email:txtEmail,
+//         salary:txtSalary
+//     }
+
+//     customerList.push(customer);
+
+//     console.log(customerList);
+// }
+
+
 let customerList = [];
 
-function btnAddCustomerOnAction(){
-    let txtName = document.getElementById("txtName").value;
-    let txtAddress = document.getElementById("txtAddress").value;
-    let txtAge = document.getElementById("txtAge").value;
-    let txtEmail = document.getElementById("txtEmail").value;
-    let txtSalary = document.getElementById("txtSalary").value;
-
-    let customer={
-        name:txtName,
-        address:txtAddress,
-        age:txtAge,
-        email:txtEmail,
-        salary:txtSalary
-    }
-
-    customerList.push(customer);
-
-    console.log(customerList);
-}
